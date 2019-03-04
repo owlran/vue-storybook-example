dcd0e4468e6349237d8d1ea2692662ef80a1c7dc
owlran <owlran1088@gmail.com>
2019-03-04 15:23:38 +0800

    chore: setup storybook environment

---
+++ src/stories/Button.vue
@@ -0,0 +1,47 @@
+<template>
+  <button class="button-styles" :style="computedStyle" @click="onClick">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+  export default {
+    name: 'my-button',
+    props: {
+      rounded: Boolean
+    },
+    methods: {
+      onClick () {
+        this.$emit('click')
+      }
+    },
+    computed: {
+      computedStyle () {
+        return {
+          borderRadius: this.rounded ? '8px' : 0
+        }
+      }
+    }
+  }
+</script>
+
+<story
+  name="Disabled Button"
+  methods="{handleClick: action('click')}"
+  knobs="{buttonText: text('Button text', 'initial value')}"
+  notes="You can't touch this">
+  <my-button @click="handleClick" :disabled="false">
+    {{ buttonText }}
+  </my-button>
+</story>
+
+<style>
+  .button-styles {
+    border: 1px solid #eee;
+    background-color: #fff;
+    cursor: pointer;
+    font-size: 15pt;
+    padding: 3px 10px;
+    margin: 10px;
+  }
+</style>
